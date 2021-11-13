a0314bbdbc4b2f2b1ee70530a5ac6202c637a006
DarkPhoenix2704 <anbarasun123@gmail.com>
2021-11-13 10:06:10 +0530

    Added Event Handler

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const {Client, Intents} = require('discord.js');
 const {token} = require('../saramma/config.json');
+const fs = require("fs");
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES],
     partials: ["CHANNEL"]
 });
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else if (client.on) {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 client.login(token);
